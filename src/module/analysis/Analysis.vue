695a668b515004f36e815e2bc1ce9626e63fdc72
kezengcai <zengcai.ke@meicloud.com>
2018-05-30 14:33:29 +0800

    add somethings

--- src/module/analysis/Analysis.vue
+++ src/module/analysis/Analysis.vue
@@ -12,87 +12,90 @@
 </template>
 
 <script>
-	import Pheader from '../../components/Pheader'
-	import { mapGetters, mapActions, mapMutations } from 'vuex'
-	export default {
-		name: 'analysis',
-		data() {
-			return {
+import Pheader from "../../components/Pheader";
+import { mapGetters, mapActions, mapMutations } from "vuex";
+export default {
+  name: "analysis",
+  data() {
+    return {};
+  },
 
-			}
-		},
-		
-		methods: {
-            init(){
-                 // 基于准备好的dom，初始化echarts实例
-        var myChart = echarts.init(document.getElementById('main'));
+  methods: {
+    init() {
+      // 基于准备好的dom，初始化echarts实例
+      var myChart = echarts.init(document.getElementById("main"));
 
-        // 指定图表的配置项和数据
-       var option1 = {
-         backgroundColor: 'yellow',
- 
-         title: {
-             text: '课程内容分布',
-             left: 'center',
-             top: 20,
-             textStyle: {
-                 color: 'red'
-             }
-         },
-         tooltip : {
-             trigger: 'item',
-             formatter: "{a} <br/>{b} : {d}%"
-         },
- 
-         // visualMap: {
-         //     show: false,
-         //     min: 500,
-         //     max: 600,
-         //     inRange: {
-         //         colorLightness: [0, 1]
-         //     }
-         // },
-         series : [
-             {
-                 name:'课程内容分布',
-                 type:'pie',
-                 clockwise:'true',
-                 startAngle:'0',
-                 radius : '50%',
-                 // center: ['50%', '50%'],
-                 data:[
-                     {
-                         value:70,
-                         name:'语言',
-           
-                     },
-                     {
-                         value:10,
-                         name:'美国科学&社会科学',
-    
-                     },
-                     {
-                         value:20,
-                         name:'美国数学',
-                     }
- 
-                 ],
-             }
-         ]
-     };
-     myChart.setOption(option1);
-            }
-		},
-		mounted() {
-            this.init()
-		},
-		components: {
-			'global-header': Pheader
-		},
-	}
+      // 指定图表的配置项和数据
+      var option1 = {
+        backgroundColor: "yellow",
+
+        title: {
+          text: "课程内容分布",
+          left: "center",
+          top: 20,
+          textStyle: {
+            color: "red"
+          }
+        },
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b} : {d}%"
+        },
+
+        // visualMap: {
+        //     show: false,
+        //     min: 500,
+        //     max: 600,
+        //     inRange: {
+        //         colorLightness: [0, 1]
+        //     }
+        // },
+        series: [
+          {
+            name: "课程内容分布",
+            type: "pie",
+            clockwise: "true",
+            startAngle: "0",
+            radius: "50%",
+            // center: ['50%', '50%'],
+            data: [
+              {
+                value: 70,
+                name: "语言"
+              },
+              {
+                value: 10,
+                name: "美国科学&社会科学"
+              },
+              {
+                value: 20,
+                name: "美国数学"
+              }
+            ]
+          }
+        ]
+      };
+      myChart.setOption(option1);
+    }
+  },
+  mounted() {
+    this.init();
+    app
+        .post({
+          url: "/php/reg.php",
+          data: { type: 'analysis' }
+        })
+        .then(data => {
+          console.log(data)
+        })
+  },
+  components: {
+    "global-header": Pheader
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang='scss' scoped>
-	@import './analysis.scss';
+@import "./analysis.scss";
 </style>
