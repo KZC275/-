b1391672ed15bb3095b1859f703d77fd50388d75
kezengcai <zengcai.ke@meicloud.com>
2018-06-02 14:54:12 +0800

    window脚本

---
+++ FTP自动上传脚本windows.cmd
@@ -0,0 +1,14 @@
+﻿rem 注释语句 ftp自动上传bat脚本
+
+
+@echo off
+echo open 60.205.39.154 >> temp.txt
+echo user bxu2442290509 aA5262325>> temp.txt
+echo bin >> temp.txt
+echo put "E:\MYPROJECT\dist\output.zip" "htdocs/output.zip" >> temp.txt
+REM echo put "E:\MYPROJECT\php\reg.php" "htdocs/php/reg.php" >> temp.txt
+echo bye >> temp.txt
+ftp -n -s:"temp.txt"
+rem del /q D:\MyData\Administrator\Desktop\dist-UAT.zip
+pause
+del /q temp.txt
