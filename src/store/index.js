4993626b8831cec9bbf983c6f9ce9fca00c9320b
kzc275@163.com <kzc275@163.com>
2018-06-29 16:50:41 +0800

    优化代码

--- src/store/index.js
+++ src/store/index.js
@@ -7,47 +7,44 @@ import myNote from '../module/myNote/store'
 Vue.use(Vuex)
 
 const state = {
-  isLogin:false,
-  username:'',
-  showMask:false,//遮罩
-  isShutDown:null,//点击遮罩回调函数
+  isLogin: false,
+  username: '',
+  showMask: false, // 遮罩
+  isShutDown: null, // 点击遮罩回调函数
   count: 18,
-  isBack:false,
-  transition:'right-to-left',
-  backfromdetail:false,//从记事详情页返回列表页
+  isBack: false,
+  transition: 'right-to-left',
+  backfromdetail: false // 从记事详情页返回列表页
 
 }
-//改变状态的事件
+// 改变状态的事件
 const mutations = {
-	isBack(){
-		state.isBack=false;
-	},
+  isBack () {
+    state.isBack = false
+  },
   increment (state) {
     console.log(this)
-    
-  		state.count++
+  	state.count++
   },
   decrement (state) {
     state.count--
   },
-  transitionChange(state,load){
-  	
-  	if(state.isBack){
-  		state.transition='left-to-right'
-  	}else{
-  		state.transition='right-to-left'
+  transitionChange (state, load) {
+  	if (state.isBack) {
+  		state.transition = 'left-to-right'
+  	} else {
+  		state.transition = 'right-to-left'
   	}
-  	
   }
 }
-//commit mutation 
+// commit mutation
 const actions = {
-  changeisBack:({commit})=>{
-  		commit('isBack');
+  changeisBack: ({commit}) => {
+  	commit('isBack')
   },
   increment_add: ({ commit }) => {
-  	commit('increment');
-  	console.log(arguments);
+  	commit('increment')
+  	console.log(arguments)
   },
   decrement_add: ({ commit }) => commit('decrement'),
   incrementIfOdd ({ commit, state }) {
@@ -68,15 +65,15 @@ const actions = {
 // getters are functions
 const getters = {
   evenOrOdd: state => state.count % 2 === 0 ? 'even' : 'odd',
-  transition:state => state.transition,
-  isBack:state => state.isBack,
-  getSrc:state => state.src,
-  getPath:state => state.path,
-  getlocationStr:state => state.locationStr,
+  transition: state => state.transition,
+  isBack: state => state.isBack,
+  getSrc: state => state.src,
+  getPath: state => state.path,
+  getlocationStr: state => state.locationStr
 }
 
 const modules = {
-	couponCenter,
+  couponCenter,
   myNote
 }
 
